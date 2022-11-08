7ecdd0323894ab424e3f4f14d819a08a57786b0f
Ramzi-Abidi <69228547+Ramzi-Abidi@users.noreply.github.com>
2022-11-08 23:14:27 +0100

    Time: 76 ms (99.55%), Space: 48.1 MB (20.68%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.js
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.js
@@ -1,18 +1,19 @@
 var numSubarrayProductLessThanK = function(arr, k) {
- if (k <= 1) return 0;
-    
-    let s = 1;
     let count = 0;
+    let s = 1;
     let j = 0;
-
+    
+    if (k <= 1)
+        return 0;
+    
     for (let i = 0; i < arr.length; i++) {
         s*= arr[i];
-        
+    
         while (s >= k) {
             s /= arr[j];
             j++;
         }
-        count += i - j + 1;
+        count += i-j+1 ;
     }
     return count;
 };
