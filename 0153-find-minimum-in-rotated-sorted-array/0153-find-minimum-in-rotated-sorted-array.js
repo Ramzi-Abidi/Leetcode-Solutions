e2de53b7258753c8a338de5d4d3954dfd651f8ff
Ramzi-Abidi <69228547+Ramzi-Abidi@users.noreply.github.com>
2022-11-11 22:18:09 +0100

    Time: 95 ms (55.17%), Space: 42.5 MB (12.67%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,23 @@
+var findMin = function(arr) {
+        let start = 0,
+        end = arr.length - 1,
+        mid ;
+    
+    if (arr[0] < arr[arr.length - 1]) {     // if the array is sorted we return the first element
+        return arr[0];
+    }
+    if(arr.length === 1) {
+        return arr[0];
+    }
+    while (start < end) {
+        mid = start + Math.floor((end - start) / 2);
+
+        if (arr[mid] < arr[end]) {
+            end = mid ;
+        } else {
+            // arr[mid] > arr[mid]
+            start = mid + 1;
+        }
+    }
+    return arr[start];
+};
