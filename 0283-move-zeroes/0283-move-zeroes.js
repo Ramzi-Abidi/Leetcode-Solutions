71ead7e6d6ef12b715e4c1c7206251a2f5bac786
Ramzi-Abidi <69228547+Ramzi-Abidi@users.noreply.github.com>
2022-11-12 10:58:36 +0100

    Time: 154 ms (61.06%), Space: 47.1 MB (18.22%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -1,17 +1,22 @@
-var moveZeroes = function(arr) {
-    let count = 0;
-    let i=0 ;
+const swap = (arr, i, j) => {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+};
 
-    while (i < arr.length) {
-        if (arr[i] === 0) {
-            arr.splice(i, 1);
-            count++;
+var moveZeroes = function (arr) {
+    let i = 0;
+    let j = 1;
+
+    while (j < arr.length) {
+        if (arr[i] === 0 && arr[j] !== 0) {
+            swap(arr, i, j);
+        } else if (arr[i] !== 0 && arr[j] !== 0) {
+            i++;
+            j++;
+        } else if (arr[i] !== 0 && arr[j] === 0) {
+            i++;
+            j++;
+        } else if (arr[i] === 0 && arr[j] === 0) {
+            j++;
         }
-        else {
-            i++ ;
-        }
-    }
-    for (let i = 0; i < count; i++) {
-        arr.push(0);
     }
 };
