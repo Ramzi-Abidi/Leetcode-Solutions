ecf0044870031a73bf816d926cba2e298b78e3c0
Ramzi-Abidi <69228547+Ramzi-Abidi@users.noreply.github.com>
2022-11-13 23:29:27 +0100

    Time: 597 ms (6.67%), Space: 49.1 MB (11.85%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.js
@@ -0,0 +1,33 @@
+function sum(arr) {
+    let s = arr[0];
+    if (arr.length === 0)
+        return 0;
+    else
+        if (arr.length === 1)
+            return arr[0];
+        else {
+            for (let i = 1; i < arr.length; i++) {
+                s ^= arr[i];
+            }
+        }
+        console.log("s:"+s);
+    return s;
+}
+
+
+var subsetXORSum = function(nums) {
+    const subSeq=(nums,prefix)=>{
+    if (nums.length === 0) {
+        s += sum(prefix);
+        return;
+    }
+    else {
+        subSeq(nums.slice(1), prefix);
+        subSeq(nums.slice(1), [...prefix, nums[0]]);
+    }
+}
+    
+    let s = 0;
+    subSeq(nums, []);
+    return s ;
+};
