0f3eb6f25e085e4ad2658fbc418349afdf7091c8
Ramzi-Abidi <69228547+Ramzi-Abidi@users.noreply.github.com>
2022-11-13 15:34:38 +0100

    Time: 535 ms (7.96%), Space: 54.5 MB (6.64%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.js
@@ -0,0 +1,23 @@
+var maxScore = function(arr, k) {
+    let j = k-1,
+        end = arr.length - 1,
+        s = 0,
+        max = 0 ;
+
+    for (let i = 0; i < k; i++) {
+        s += arr[i];
+    }
+
+    max = Math.max(max, s);
+
+    for (let i = 0; i < k; i++) {
+        s -= arr[j];
+        j--;
+        s += arr[end];
+        end--;
+        console.log(max)
+        max = Math.max(max, s);
+    }
+
+    return max;
+};
