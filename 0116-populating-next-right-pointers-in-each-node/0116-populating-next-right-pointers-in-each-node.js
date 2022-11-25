f36f4820e0caf95afd701ecbb4d114906478bcc3
Ramzi-Abidi <69228547+Ramzi-Abidi@users.noreply.github.com>
2022-11-25 17:10:37 +0100

    Time: 137 ms (39.87%), Space: 48.4 MB (83.74%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.js
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.js
@@ -1,17 +1,3 @@
-/**
- * // Definition for a Node.
- * function Node(val, left, right, next) {
- *    this.val = val === undefined ? null : val;
- *    this.left = left === undefined ? null : left;
- *    this.right = right === undefined ? null : right;
- *    this.next = next === undefined ? null : next;
- * };
- */
-
-/**
- * @param {Node} root
- * @return {Node}
- */
 var connect = function(root) {
     let ptr = root;
     while(root && root.left){
