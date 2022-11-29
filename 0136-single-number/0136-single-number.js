775a962d235c416992b6893338466f52c282fda7
Ramzi-Abidi <69228547+Ramzi-Abidi@users.noreply.github.com>
2022-11-29 15:02:22 +0100

    Time: 119 ms (52.74%), Space: 47.4 MB (26.55%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,18 @@
+var singleNumber = function (arr) {
+    let hash = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        if (hash[arr[i]] === undefined) {
+            hash[arr[i]] = 1;
+        } else {
+            hash[arr[i]]++;
+        }
+    }
+
+
+    for (const key in hash) {
+        if(hash[key] === 1) {
+            return key ;
+         }
+    }
+};
