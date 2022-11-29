69acc71b2aed71f811f5ed573a5ce8e095f21640
Ramzi-Abidi <69228547+Ramzi-Abidi@users.noreply.github.com>
2022-11-29 14:13:18 +0100

    Time: 2869 ms (15.13%), Space: 48.3 MB (20.17%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.js
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.js
@@ -1,34 +1,39 @@
 var getMaximumGold = function (grid) {
-
+    // exploring all paths + backtracking technique :
     function dfs(grid, i, j, s) {
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === 0) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] === 0) {
             return;
         }
 
-        if (visited[i][j]) {
+        let ch = i + "," + j ;
+
+         if (visited.has(ch)) {
             return;
         }
-        visited[i][j] = true;
+
+        visited.add(ch) ;
 
         s += grid[i][j];
 
         max = Math.max(max, s);
 
-        dfs(grid, i + 1, j, s);
-        dfs(grid, i - 1, j, s);
-        dfs(grid, i, j + 1, s);
-        dfs(grid, i, j - 1, s);
+        //right
+        dfs(grid, i, j + 1,s);
+        //top
+        dfs(grid, i - 1, j,s);
+        //bottom
+        dfs(grid, i + 1, j,s);
+        //left
+        dfs(grid, i, j - 1,s);
 
-        visited[i][j] = false;
+        visited.delete(ch);
     }
 
-    let m = grid.length;
-    let n = grid[0].length;
-    let visited = new Array(m).fill(false).map((val) => new Array(n).fill(val));
+    let visited = new Set() ;
     let max = 0;
 
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
             if (grid[i][j] !== 0) {
                 dfs(grid, i, j, 0);
             }
