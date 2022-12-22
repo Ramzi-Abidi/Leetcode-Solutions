5f389b8ae3d3ec77cd4261091eae4c9565ed77d0
Ramzi-Abidi <69228547+Ramzi-Abidi@users.noreply.github.com>
2022-12-23 00:25:05 +0100

    Time: 128 ms (57.55%), Space: 45.7 MB (52.15%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -1,12 +1,59 @@
-var maxAreaOfIsland = function(grid) {
-    let ans = 0, n = grid.length, m = grid[0].length
-    const trav = (i, j) => {
-        if (i < 0 || j < 0 || i >= n || j >= m || !grid[i][j]) return 0
-        grid[i][j] = 0
-        return 1 + trav(i-1, j) + trav(i, j-1) + trav(i+1, j) + trav(i, j+1)
+const dfs = (grid, i, j, visited, t) => {
+    //out of the grid
+    if (i < 0 || i >= grid.length || j >= grid[0].length || j < 0) {
+        return 0;
     }
-    for (let i = 0; i < n; i++) 
-        for (let j = 0; j < m; j++)
-            if (grid[i][j]) ans = Math.max(ans, trav(i, j))
-    return ans
+
+    //not land
+    if (grid[i][j] === 0) {
+        return 0;
+    }
+
+    //already visited cell
+    if (visited[i][j]) {
+        return 0;
+    }
+
+    let count = 0;
+
+    if (grid[i][j] === 1) {
+        count += 1;
+    }
+
+    // visited cell
+    visited[i][j] = true;
+
+    // top
+    count += dfs(grid, i - 1, j, visited);
+    //right
+    count += dfs(grid, i, j + 1, visited);
+    //left
+    count += dfs(grid, i, j - 1, visited);
+    // bottom
+    count += dfs(grid, i + 1, j, visited);
+
+    return count;
+};
+
+var maxAreaOfIsland = function (grid) {
+    // visited
+
+    let visited = [];
+    for (let i = 0; i < grid.length; i++) {
+        let arr = [];
+        for (let j = 0; j < grid[0].length; j++) {
+            arr.push(false);
+        }
+        visited.push(arr);
+    }
+
+    let max = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] === 1) {
+                max = Math.max(max, dfs(grid, i, j, visited));
+            }
+        }
+    }
+    return max;
 };
