a333783ac9d662deb06bcda4ab90606af9ccdd39
Abidi Ramzi <69228547+Ramzi-Abidi@users.noreply.github.com>
2024-02-27 12:32:03 +0100

    Time: 47 ms (97.69%), Space: 51.8 MB (47.26%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,18 @@
+var fizzBuzz = function (n) {
+    let res = [];
+
+    for (let i = 0; i < n; i++) {
+        if ((i + 1) % 3 === 0 && (i + 1) % 5 === 0) {
+            res.push("FizzBuzz");
+        }
+        else if ((i + 1) % 3 === 0) {
+            res.push("Fizz");
+        }
+        else if ((i + 1) % 5 === 0) {
+            res.push("Buzz");
+        } else {
+            res.push(String(i + 1));
+        }
+    }
+    return res;
+};
